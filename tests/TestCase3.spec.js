2eafc9ace17a4980827a1b9700695dff31c61e91
John <thapajohn2057@gmail.com>
2025-07-08 16:41:02 +0545

    incorrect password and email error message

--- tests/TestCase3.spec.js
+++ tests/TestCase3.spec.js
@@ -1,20 +1,20 @@
-const{test, expect}= require('@playwright/test');
+const { test, expect } = require('@playwright/test');
 
-test('Login User with incorrect email and password',async({page})=>{
-
+test('Login User with incorrect email and password', async ({ page }) => {
     await page.goto('https://automationexercise.com/');
 
     const logo = page.locator('img[alt="Website for automation practice"]');
     await expect(logo).toBeVisible();
-  
+
     await page.locator('text="Signup / Login"').click();
 
-    const LoginTitle= page.locator('h2:text("Login to your Account")');
-    await expect(LoginTitle).toBeVisible;
+    const LoginTitle = page.locator('h2:has-text("Login to your account")');
+    await expect(LoginTitle).toBeVisible(); 
 
-    await page.locator('[data-qa="login-email"]').fill("test123@gmail.com")
-    await page.locator('[data-qa="login-password"]').fill("jhsd5325sdb")
+    await page.locator('[data-qa="login-email"]').fill("test123@gmail.com");
+    await page.locator('[data-qa="login-password"]').fill("jhsd5325sdb");
 
     await page.locator('[data-qa="login-button"]').click();
-    
-})
+
+    const errorMsg = page.locator('p', { hasText: 'Your email or password is incorrect!' });
+    await expect(errorMsg).toBeVisible(); 
